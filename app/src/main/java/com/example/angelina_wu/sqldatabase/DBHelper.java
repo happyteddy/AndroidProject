5a8f746774cc904a7368615557ac3b54f8fcfd8e
Angelina Wu(吳家禎) <Angelina_Wu@corpnet.asus>
2016-04-25 09:25:26 +0800

    version1

---
+++ app/src/main/java/com/example/angelina_wu/sqldatabase/DBHelper.java
@@ -0,0 +1,38 @@
+package com.example.angelina_wu.sqldatabase;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import static android.provider.BaseColumns._ID;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_NAME = "information";
+    public static final String USER_ID = "id";
+    public static final String USER_NAME = "name";
+    private static final int VERSION = 1;
+    private final static String DATABASE_NAME = "info.db";
+
+    //constructor
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+    public DBHelper(Context context, String name) {
+        this(context, name, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String INIT_TABLE = "CREATE TABLE " + TABLE_NAME + " ("+_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +  USER_NAME + " CHAR);" ;
+        db.execSQL(INIT_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(DROP_TABLE);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/angelina_wu/sqldatabase/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.angelina_wu.sqldatabase;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.BaseColumns._ID;
+import static com.example.angelina_wu.sqldatabase.DBHelper.TABLE_NAME;
+import static com.example.angelina_wu.sqldatabase.DBHelper.USER_NAME;
+
+public class MainActivity extends AppCompatActivity {
+    private DBHelper helper = null;
+    TextView result = null;
+    int userId = 1 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = new DBHelper(this);
+        helper.close();
+        result = (TextView) findViewById(R.id.showData);
+    }
+
+    public void add(View view) {
+        EditText userName = (EditText) findViewById(R.id.enterUserName);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(USER_NAME, userName.getText().toString());
+        db.insert(TABLE_NAME, null, values);
+        userId = userId + 1 ;
+    }
+
+    public void show(View view) {
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String[] projection = { _ID , USER_NAME  };
+        Cursor c = db.query( TABLE_NAME, projection, null, null, null, null, null );
+
+        StringBuilder resultData = new StringBuilder("RESULT: \n");
+        while(c.moveToNext()){
+            int id = c.getInt(0);
+            String name = c.getString(1);
+            resultData.append(id).append(": ");
+            resultData.append(name).append(", ");
+            resultData.append("\n");
+        }
+        result.setText(resultData);
+    }
+}
